87bfe77ce7cc5990557b020a077ccbeb3e8939f4
jjanczynska <joaxjan@gmail.com>
2023-11-30 15:01:52 +0000

    Complete checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,7 +14,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag',
+                       'stripe_pid', )
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
@@ -24,7 +25,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
-                    'grand_total',)
+                    'grand_total', 'original_bag',
+                       'stripe_pid')
 
     ordering = ('-date',)
 
